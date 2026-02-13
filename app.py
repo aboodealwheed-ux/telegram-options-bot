9345f8ab7ac0aea75b6ac8da288733a1e55caf06
aboodealwheed-ux <aboodealwheed@gmail.com>
2026-02-14 01:39:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-TOKEN = os.environ.get("BOT_TOKEN")
-TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+TELEGRAM_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
 
 @app.route("/", methods=["GET"])
 def home():
     return "Bot is running"
 
-@app.route(f"/{TOKEN}", methods=["POST"])
+
+@app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
 
@@ -31,5 +33,6 @@ def webhook():
 
     return "ok"
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
